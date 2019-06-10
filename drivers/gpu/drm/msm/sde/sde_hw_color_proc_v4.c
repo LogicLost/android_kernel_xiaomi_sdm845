850e7dbb8d95b9c86b4c46f77a2bd7afb9199689
LogicLost <logiclost00@gmail.com>
2019-06-10 04:37:11 -0400

    drm: msm: kcal:Set default to 270 saturation
    Keep the colors vibrant and accurate.

--- drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
+++ drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
@@ -19,7 +19,7 @@ static unsigned short kcal_red = 256;
 static unsigned short kcal_green = 256;
 static unsigned short kcal_blue = 256;
 static unsigned short kcal_hue = 0;
-static unsigned short kcal_sat = 255;
+static unsigned short kcal_sat = 270;
 static unsigned short kcal_val = 255;
 static unsigned short kcal_cont = 255;
 
