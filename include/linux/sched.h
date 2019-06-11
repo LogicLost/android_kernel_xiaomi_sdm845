dc2af2122b30c2d8ad05f0ef345c081b7e086b65
LogicLost <logiclost00@gmail.com>
2019-06-11 08:02:02 -0400

    Revert "sched/tune: Add support for dynamic prefer_idle"
    This reverts commit e164c910c702c42b8174fb508e4987c0adec7c41.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -4029,12 +4029,6 @@ void cpufreq_remove_update_util_hook(int cpu);
 int do_stune_boost(char *st_name, int boost, int *slot);
 int do_stune_sched_boost(char *st_name, int *slot);
 int reset_stune_boost(char *st_name, int slot);
-int do_prefer_idle(char *st_name, u64 prefer_idle);
-
-static inline int do_prefer_idle(char *st_name, u64 prefer_idle)
-{
-	return 0;
-}
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 extern DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
